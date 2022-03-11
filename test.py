8ecd8600a4f57e0878904313485b57a611694b71
xXCaLXx <calinwoods@gmail.com>
2022-03-11 11:09:27 +1000

    ladder notebook

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-print("Hello World")
-    #testing branches
+import numpy as np
+import matplotlib 
+import matplotlib as mpl
+import astropy
+import os
+#from tabulate import tabulate 
+
+from astropy.timeseries import LombScargle
+from astropy.table import Table
+
+with open('/Users/calinperez/Desktop/PHYS3080/Untitled/U07/xrfdata.txt', 'r') as f:
+
+    xray = f.read()
+    #print(tabulate(xray, headers=['x', 'y', 'z']))
+    print(xray)
+    #matplotlib.pyplot.scatter(xray)
